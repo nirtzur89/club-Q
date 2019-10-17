6f63ad2588cd7c1337af6f2988c32357083e6399
nirtzur <nirtzur89@gmail.com>
2019-10-17 23:13:26 +0200

    updates passed with club filter

--- src/components/ClubPage.js
+++ src/components/ClubPage.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import getVisibleUpdates from '../selectors/updates';
 
 const ClubPage = props => {
-  console.log('thissss', props.match.params.id);
+  console.log('show visible', props.updates);
   return <div>this is edit for: {props.match.params.id}</div>;
 };
 
-export default ClubPage;
+const mapStateToProps = (state, props) => {
+  const club = props.match.params.id;
+  return {
+    updates: getVisibleUpdates(state.updates, { club })
+  };
+};
+
+export default connect(mapStateToProps)(ClubPage);
