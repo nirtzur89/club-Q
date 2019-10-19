cfcbdcad911deb3493fe6af8bbbd6e112b56ccf2
nirtzur <nirtzur89@gmail.com>
2019-10-19 23:05:59 +0200

    webpack - extract-text-webpack-plugin

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/configureStore';
-import './styles/styles.scss';
 import AppRouter from './routers/AppRouter';
 import { Provider } from 'react-redux';
+
+import './styles/styles.scss';
 
 //redux imports
 import { addUpdate } from './actions/updates';
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,36 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        loader: 'babel-loader',
-        test: /\.js$/,
-        exclude: /node_modules/
-      },
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
-  }
+module.exports = env => {
+  const isProduction = env === 'production';
+  const CSSExtract - new ExtractTextPlugin('styles.css');
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/
+        },
+        {
+          test: /\.s?css$/,
+          use: CSSExtract.extract({
+            use: [ 'css-loader', 'sass-loader']
+          })
+        }
+      ]
+    },
+    plugins: [CSSExtract],
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true
+    }
+  };
 };
+// ['style-loader', 'css-loader', 'sass-loader']
